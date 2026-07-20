549e07d52957b56ba2f2d124b659161f9312a7e4
UnarineLeo <unarineleo@gmail.com>
2026-07-20 06:20:29 +0200

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'the.growth.system',
+  appName: 'the-growth-system',
+  webDir: 'www'
+};
+
+export default config;
